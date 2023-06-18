971c1bc4c5f1d023df2f753ebd20b44a3e5f6d9d
Rishi Raj Singh <78776240+RishiRajSingh1@users.noreply.github.com>
2023-06-18 17:53:27 +0530

    First Commit

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import App from '../App.css'
+
+    
+function Header() {
+  return (
+    <div>
+      <nav>Complete Your Daily Tasks.</nav>
+    </div>
+  )
+}
+
+export default Header
